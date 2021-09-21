137dc224ca6071a21057e88fa518514160d8fd6a
spencerh724 <90658660+spencerh724@users.noreply.github.com>
2021-09-21 11:59:40 -0400

    update 11:59am

--- main.cpp
+++ main.cpp
@@ -25,6 +25,28 @@ class Matrix3x4{
             cout << endl;
         }
 
+        Matrix3x4(string s){
+            rows = 3;
+            columns = 4;
+            table = new double*[rows];
+            for(int i = 0; i < rows; i++){
+                table[i] = new double[columns];
+            }
+            table[0][0] = 2;
+            table[0][1] = 6;
+            table[0][2] = 1;
+            table[0][3] = 7;
+            table[1][0] = 1;
+            table[1][1] = 2;
+            table[1][2] = -1;
+            table[1][3] = -1;
+            table[2][0] = 5;
+            table[2][1] = 7;
+            table[2][2] = -4;
+            table[2][3] = 9;
+
+        }
+
         string toString(){
             string ret = "";
             for(int i = 0; i < rows; i++){
@@ -35,7 +57,7 @@ class Matrix3x4{
             }
             return ret;
         }
-    
+
         void scaleRow(int r, double k){
             for(int i = 0; i < columns; i++){
                 table[r][i] *= k;
@@ -44,16 +66,65 @@ class Matrix3x4{
 
         void addRow(int k, int rA, int rB){
             for(int i = 0; i < columns; i++){
-
+                table[rB][i] += k * table[rA][i];
             }
+        }
+
+        void RREF(){
+            /*
+            for(int i = 0; i < columns; i++){
+                scaleRow(i, 1.0/table[i][i]);
+                for(int j = 0; j < rows; j++){
+                    if(j != i){
+                        addRow(-(table[j][i]), i, j);
+                    }
+                }
+            }
+            */
         }
 };
 
 
 int main(){
-    Matrix3x4 A;
-    A.scaleRow(0, 0.5);
-    cout << A.toString();
+    Matrix3x4 A("akdslfjfhfj");
+    cout << A.toString() << endl;
+
+    A.scaleRow(0, 1.0/A.table[0][0]);
+    cout << "cout << A.toString() << endl;" << endl;
+    cout << A.toString() << endl;
+
+    A.addRow(-(A.table[1][0]), 0, 1);
+    cout << "A.addRow(-(A.table[1][0]), 0, 1);" << endl;
+    cout << A.toString() << endl;
+
+    A.addRow(-(A.table[2][0]), 0, 2);
+    cout << "A.addRow(-(A.table[2][0]), 0, 2);" << endl;
+    cout << A.toString() << endl;
+    //////////////////////////////////////////////
+    A.scaleRow(1, 1.0/A.table[1][1]);
+    cout << "A.scaleRow(1, 1.0/A.table[1][1]);" << endl;
+    cout << A.toString() << endl;
+
+    A.addRow(-(A.table[0][1]), 1, 0);
+    cout << "A.addRow(-(A.table[0][1]), 0, 1);" << endl;
+    cout << A.toString() << endl;
+
+    A.addRow(-(A.table[2][1]), 1, 2);
+    cout << "A.addRow(-(A.table[2][1]), 0, 2);" << endl;
+    cout << A.toString() << endl;
+    //////////////////////////////////////////////
+    A.scaleRow(2, 1.0/A.table[2][2]);
+    cout << "A.scaleRow(2, 1.0/A.table[2][2]);" << endl;
+    cout << A.toString() << endl;
+
+    A.addRow(-(A.table[0][2]), 2, 0);
+    cout << "A.addRow(-(A.table[0][3]), 2, 0);" << endl;
+    cout << A.toString() << endl;
+
+    A.addRow(-(A.table[1][2]), 2, 1);
+    cout << "A.addRow(-(A.table[1][3]), 2, 1);" << endl;
+    cout << A.toString() << endl;
+
 
 }
 
